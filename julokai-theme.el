07054f35c4040b0091c0ac5d23914218423ce261
Julien Blanchard <julien@sideburns.eu>
2013-07-10 11:20:41 +0200

    Moar colors, add line breaks

--- julokai-theme.el
+++ julokai-theme.el
@@ -38,6 +38,7 @@
       (cold-mud          "#cebca5")
 
       (hard-pink         "#f92672")
+      (toulouse-violet   "#9c71a5")
       (light-magenta     "#d33682")
       (dark-magenta      "#960050")
       (light-yellow      "#e6db74")
@@ -45,7 +46,10 @@
       (desert            "#cebca5")
       (orange            "#fd971f")
       (light-orange      "#f9b529")
-      (dark-grey         "#303347")
+      (grey              "#808080")
+      (dark-grey         "#403d3d")
+      (dark-blue-grey    "#303347")
+      (turquoise         "#66d9ef")
 
       (full-white        "#ffffff")
       (full-black        "#000000")
@@ -58,49 +62,89 @@
 
 (custom-theme-set-faces
  'julokai
- '(default ((t (:family "Source_Code_Pro" :foundry "apple" :width normal :height 140 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#f8f8f0" :background "#1b1d1e" :stipple nil :inherit nil))))
- '(cursor ((t (:foreground "#1b1d1e" :background "#f8f8f0"))))
- '(fixed-pitch ((t (:family "Monospace"))))
- '(variable-pitch ((t (:family "Sans Serif"))))
- '(escape-glyph ((t (:weight bold :foreground "#cebca5"))))
- '(minibuffer-prompt ((t (:weight bold :foreground "#9c71a5"))))
- '(highlight ((t (:background "#808080"))))
- '(vertical-border ((t (:foreground "#303347"))))
- '(region ((t (:foreground "#ffffff" :background "#403d3d"))))
+ '(default
+   ((t (:family "Source_Code_Pro" :foundry "apple" :width normal :height 140 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#f8f8f0" :background "#1b1d1e" :stipple nil :inherit nil))))
+ '(cursor
+   ((t (:foreground "#1b1d1e" :background "#f8f8f0"))))
+ '(fixed-pitch
+   ((t (:family "Monospace"))))
+ '(variable-pitch
+   ((t (:family "Sans Serif"))))
+ '(escape-glyph
+   ((t (:weight bold :foreground "#cebca5"))))
+ '(minibuffer-prompt
+   ((t (:weight bold :foreground "#9c71a5"))))
+ '(highlight
+   ((t (:background "#808080"))))
+ '(vertical-border
+   ((t (:foreground "#303347"))))
+ '(region
+   ((t (:foreground "#ffffff" :background "#403d3d"))))
  '(shadow ((((class color grayscale) (min-colors 88) (background light)) (:foreground "grey50")) (((class color grayscale) (min-colors 88) (background dark)) (:foreground "grey70")) (((class color) (min-colors 8) (background light)) (:foreground "green")) (((class color) (min-colors 8) (background dark)) (:foreground "yellow"))))
  '(secondary-selection ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "SkyBlue4")) (((class color) (min-colors 16) (background light)) (:background "yellow")) (((class color) (min-colors 16) (background dark)) (:background "SkyBlue4")) (((class color) (min-colors 8)) (:foreground "black" :background "cyan")) (t (:inverse-video t))))
- '(trailing-whitespace ((t (:underline (:color foreground-color :style line) :background "red1" :inherit (show-paren-mismatch-face)))))
- '(font-lock-builtin-face ((t (:foreground "#a6e22e"))))
- '(font-lock-comment-delimiter-face ((t (:foreground "#465457"))))
- '(font-lock-comment-face ((t (:foreground "#465457"))))
- '(font-lock-constant-face ((t (:weight bold :slant italic :foreground "#ae81ff"))))
- '(font-lock-doc-face ((t (:slant italic :foreground "#9c71a5" :inherit (font-lock-string-face)))))
- '(font-lock-function-name-face ((t (:foreground "#a6e22e"))))
- '(font-lock-keyword-face ((t (:weight bold :foreground "#f92672"))))
- '(font-lock-negation-char-face ((t (:weight bold :foreground dark-magenta))))
- '(font-lock-preprocessor-face ((t (:foreground "#cebca5" :inherit (font-lock-builtin-face)))))
- '(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
- '(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
- '(font-lock-string-face ((t (:foreground "#e6db74"))))
- '(font-lock-type-face ((t (:weight bold :foreground "#66d9ef"))))
- '(font-lock-variable-name-face ((t (:foreground "#fd971f"))))
- '(font-lock-warning-face ((t (:weight bold :foreground "#d33682"))))
- '(button ((t (:inherit (link)))))
- '(link ((t (:weight bold :underline (:color foreground-color :style line) :foreground "#9dbbd3"))))
- '(link-visited ((default (:inherit (link))) (((class color) (background light)) (:foreground "magenta4")) (((class color) (background dark)) (:foreground "violet"))))
- '(fringe ((t (:background "#1b1d1e" :foreground "#455354"))))
- '(header-line ((t (:weight bold :box nil :foreground "#ffffff" :background "#232533" :inherit (mode-line)))))
- '(tooltip ((((class color)) (:inherit (variable-pitch) :foreground "black" :background "lightyellow")) (t (:inherit (variable-pitch)))))
- '(mode-line ((t (:weight bold :box nil :foreground "#f8f8f0" :background "#293739"))))
- '(mode-line-buffer-id ((t (:weight bold))))
- '(mode-line-emphasis ((t (:weight bold))))
+ '(trailing-whitespace
+   ((t (:underline (:color foreground-color :style line) :background "red1" :inherit (show-paren-mismatch-face)))))
+ '(font-lock-builtin-face
+   ((t (:foreground "#a6e22e"))))
+ '(font-lock-comment-delimiter-face
+   ((t (:foreground "#465457"))))
+ '(font-lock-comment-face
+   ((t (:foreground "#465457"))))
+ '(font-lock-constant-face
+   ((t (:weight bold :slant italic :foreground "#ae81ff"))))
+ '(font-lock-doc-face
+   ((t (:slant italic :foreground "#9c71a5" :inherit (font-lock-string-face)))))
+ '(font-lock-function-name-face
+   ((t (:foreground "#a6e22e"))))
+ '(font-lock-keyword-face
+   ((t (:weight bold :foreground "#f92672"))))
+ '(font-lock-negation-char-face
+   ((t (:weight bold :foreground dark-magenta))))
+ '(font-lock-preprocessor-face
+   ((t (:foreground "#cebca5" :inherit (font-lock-builtin-face)))))
+ '(font-lock-regexp-grouping-backslash
+   ((t (:inherit (bold)))))
+ '(font-lock-regexp-grouping-construct
+   ((t (:inherit (bold)))))
+ '(font-lock-string-face
+   ((t (:foreground "#e6db74"))))
+ '(font-lock-type-face
+   ((t (:weight bold :foreground "#66d9ef"))))
+ '(font-lock-variable-name-face
+   ((t (:foreground "#fd971f"))))
+ '(font-lock-warning-face
+   ((t (:weight bold :foreground "#d33682"))))
+ '(button
+   ((t (:inherit (link)))))
+ '(link
+   ((t (:weight bold :underline (:color foreground-color :style line) :foreground "#9dbbd3"))))
+ '(link-visited
+   ((default (:inherit (link))) (((class color) (background light)) (:foreground "magenta4")) (((class color) (background dark)) (:foreground "violet"))))
+ '(fringe
+   ((t (:background "#1b1d1e" :foreground "#455354"))))
+ '(header-line
+   ((t (:weight bold :box nil :foreground "#ffffff" :background "#232533" :inherit (mode-line)))))
+ '(tooltip
+   ((((class color)) (:inherit (variable-pitch) :foreground "black" :background "lightyellow")) (t (:inherit (variable-pitch)))))
+ '(mode-line
+   ((t (:weight bold :box nil :foreground "#f8f8f0" :background "#293739"))))
+ '(mode-line-buffer-id
+   ((t (:weight bold))))
+ '(mode-line-emphasis
+   ((t (:weight bold))))
  '(mode-line-highlight ((((class color) (min-colors 88)) (:box (:line-width 2 :color "grey40" :style released-button))) (t (:inherit (highlight)))))
- '(mode-line-inactive ((t (:box nil :background "#455354" :foreground "#f8f8f0"))))
- '(isearch ((t (:weight bold :background "#c4be89" :foreground "#000000"))))
- '(isearch-fail ((t (:background "#1e0010" :foreground dark-magenta))))
- '(lazy-highlight ((t (:weight bold :foreground "#f9b529" :background "#2a2c3e"))))
+ '(mode-line-inactive
+   ((t (:box nil :background "#455354" :foreground "#f8f8f0"))))
+ '(isearch
+   ((t (:weight bold :background "#c4be89" :foreground "#000000"))))
+ '(isearch-fail
+   ((t (:background "#1e0010" :foreground dark-magenta))))
+ '(lazy-highlight
+   ((t (:weight bold :foreground "#f9b529" :background "#2a2c3e"))))
  '(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground "white" :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
- '(next-error ((t (:inherit (region)))))
- '(query-replace ((t (:inherit (lazy-highlight))))))
+ '(next-error
+   ((t (:inherit (region)))))
+ '(query-replace
+   ((t (:inherit (lazy-highlight))))))
 
 (provide-theme 'julokai)
