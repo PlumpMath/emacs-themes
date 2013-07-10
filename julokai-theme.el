2423fe66eb802ae6f320d132d3458de4d26a4dd0
Julien Blanchard <julien@sideburns.eu>
2013-07-10 10:49:31 +0200

    Add header comments

--- julokai-theme.el
+++ julokai-theme.el
@@ -1,3 +1,23 @@
+;;; julokai-theme.el --- Nice looking emacs 24 theme
+
+;; Copyright 2013 Julien Blanchard
+
+;; Author: Julien Blanchard <julien@sideburns.eu>
+;; URL: https://github.com/julienXX/julokai
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
 (deftheme julokai
   "Created 2013-07-02.")
 
